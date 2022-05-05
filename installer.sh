a13752cfa2df0e5877280b3203e6389c412cd0bb
Alexander <agorshkov@mznx.ru>
2022-05-05 13:55:21 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 if [ "$EUID" -ne 0 ]
     then echo "Please run as root"
     exit
@@ -12,7 +14,9 @@ apt upgrade -y
 echo "Installing tilda"
 apt install -y tilda
 
-# put config to home directories
+for d in /home/*/ ; do
+	wget -O "$d.config/tilda/config_0" https://raw.githubusercontent.com/mazanax/bebian/master/.config/tilda/config_0
+done
 
 echo "Installing nvim"
 apt install -y neovim
@@ -27,3 +31,6 @@ snap install telegram-desktop
 
 # create Telegram desktop icon in all home directories
 # wget -O /home/mazanax/Desktop/Telegram.desktop https://raw.githubusercontent.com/mazanax/bebian/master/Desktop/Telegram.desktop
+for d in /home/*/ ; do
+	wget -O "$dDesktop/Telegram.desktop" https://raw.githubusercontent.com/mazanax/bebian/master/.config/tilda/config_0
+done
